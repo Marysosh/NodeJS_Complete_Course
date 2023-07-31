b1c25cc703a0bd45d9fbb0ff3747da9a747d5531
Mariia Soshnikova <soshnikova.m98@gmail.com>
2023-07-31 22:11:44 +0400

    section-3: routing requests

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const http = require("http");
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if(url === "/") {
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+
+        //return is added to quit all big createServer callback function execution
+        return res.end();
+    }
+
 
     // quits the server
     // process.exit();
 
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My Test Page</title></head>");
-    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-    res.write("</html>");
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My Test Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
 
     //we mustn't change the response after res.end();
     res.end(); 
